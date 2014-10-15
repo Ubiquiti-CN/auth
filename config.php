a119f7d909c5fa9c92c3a122b0d62273637821f9
Sid <sid@ubnt.com>
2014-10-15 13:26:01 +0800

    test

--- config.php
+++ config.php
@@ -1,57 +1,11 @@
 <?php
 
-//UniFi 微信和验证码授权上网配置信息
-
-//-----------------------------------服务器配置-------------------------------------------------
-define('SERVER_HOST', '');
-//-----------------------------------服务器配置-------------------------------------------------
-
 //-----------------------------------UniFi----------------------------------------------------
 /* UniFi 配置 */
 define('UNIFI_SERVER', '');
 define('UNIFI_USER', '');
 define('UNIFI_PASSWORD', '');
-
-/* wifi有效时间 */
-define('WIFI_EXPIRED_TIME', 60);//分钟
 //-----------------------------------UniFi----------------------------------------------------
-
-//-----------------------------------微信------------------------------------------------------
-/* 默认跳转页面 */
-define('DEFAULT_URL', 'http://www.ubnt.com.cn');
-
-/* 微信 开发者中心->服务器配置 Token值 */
-define('WECHAT_TOKEN', '');
-
-/* 微信消息和欢迎内容 */
-define('WEIXIN_AUTH_MESSAGE', '');//当用户发送此内容才能获取返回
-define('WEIXIN_ADD_WELCOME_MESSAGE', '');//关注后的欢迎内容
-//-----------------------------------微信------------------------------------------------------
-
-//-----------------------------------微博------------------------------------------------------
-define('WEIBO_AKEY', '');
-define('WEIBO_SKEY', '');
-define('WEIBO_CALLBACK_URL', '');
-define('WEIBO_NAME', '');
-define('WEIBO_MESSAGE', '');
-define('WEIBO_SEND_ERROR_MESSAGE', '');
-define('WEIBO_FOLLOW_ERROR_MESSAGE', '');
-//-----------------------------------微博------------------------------------------------------
-
-//-----------------------------------MySQL----------------------------------------------------
-/* 配置mysql */
-define('DB_HOST', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');//数据库密码
-define('DB_DBNAME', '');//数据库库名
-define('DB_PORT', '3306');
-
-define('WEIXIN_TABLE', '');//微信表名
-define('VERIFY_CODE_TABLE', '');//验证码表名
-//-----------------------------------MySQL----------------------------------------------------
-
-
-
 
 //-----------------------------------系统配置（不要修改）-----------------------------------------
 define('ROOT_PATH', dirname(__FILE__));
@@ -63,8 +17,12 @@ define('WEIBO_PATH', SDK_PATH . '/weibo');
 /* cookie 存放路径 */
 define('COOKIE_FILE_PATH', ROOT_PATH . '/tmp/unifi_cookie');
 
+//UniFi 微信和验证码授权上网配置信息
+define('CONFIG_PATH', ROOT_PATH . '/config');
+
 include_once (DEPS_PATH . '/VerifyCode.php');
 include_once (DEPS_PATH . '/unifi.php');
+include_once (DEPS_PATH . '/get_site.php');
 
 include_once (DEPS_PATH . '/mysql.php');
 $config = array(
---
+++ s/weixin/weixin.php
@@ -0,0 +1,51 @@
+<?php
+
+include_once ('../../config.php');
+include_once (WEIXIN_PATH . '/class/wechat.class.php');
+
+$options = array(
+    'token' => WECHAT_TOKEN,
+);
+$weObj = new Wechat($options);
+$weObj->valid();
+
+$rev = $weObj->getRev();
+$type = $rev->getRevType();
+$revEvent = $rev->getRevEvent();
+$event = $revEvent['event'];
+$fromUserName = $rev->getRevFrom();
+switch($type) {
+    case Wechat::MSGTYPE_TEXT:
+        $content = $rev->getRevContent();
+        if ($content == WEIXIN_AUTH_MESSAGE) {
+            $text = "<a href='" . SERVER_HOST . "/guest/sdk/weixin/redirct.php?fromUserName=" . $fromUserName . "'>点击上网</a>";
+            $weObj->text($text)->reply();
+        }
+        break;
+    case Wechat::MSGTYPE_EVENT:
+        if ($event == 'subscribe') {  //关注微信操作
+            $weObj->text(WEIXIN_ADD_WELCOME_MESSAGE)->reply();
+        } else if ($event == 'unsubscribe') {  //取消关注微信操作
+            //取消上网权限
+            $sql = "select * from " . WEIXIN_TABLE . " WHERE `fromUserName` = '{$fromUserName}'";
+            $res = $mysql->query($sql, 'all');
+
+            if (is_array($res) && count($res) > 0) {
+                //删除数据
+                $sql = "DELETE FROM " . WEIXIN_TABLE . " WHERE `fromUserName` = '{$fromUserName}'";
+                $mysql->query($sql);
+
+                foreach ($res as $key => $value) {
+                    UniFi::sendUnauthorization($value['Mac_ID']);
+                    sleep(5);
+                }
+            }
+        }
+        break;
+    case Wechat::MSGTYPE_IMAGE:
+
+        break;
+    default:
+        $weObj->text("help info")->reply();
+        break;
+}
