9e254798e8910b6e510cea445758f05315036634
Eric Zhao <zhaoyijun1987@hotmail.com>
2014-10-17 11:33:08 +0800

    add qq

---
+++ s/qq/callback.php
@@ -0,0 +1,28 @@
+<?php
+
+session_start();
+
+include_once('../../config.php');
+$site = get_site(__DIR__);
+include_once (CONFIG_PATH . '/' . $site . PHP_EXT);
+include_once(QQ_PATH . '/qqConnectAPI.php');
+
+$qc = new QC();
+$acs = $qc->qq_callback();
+$oid = $qc->get_openid();
+$qc = new QC($acs,$oid);
+$user = $qc->get_user_info();
+$idol = array (
+    'name' => QQ_ADD_IDOL_BY_NAME
+);
+$content = array(
+    'content' => QQ_SEND_WEIBO_MESSAGE
+);
+
+$idol_ret = $qc->add_idol($idol);
+$content_ret = $qc->add_t($content);
+
+UniFi::set_site($site);
+UniFi::sendAuthorization($Mac_ID, WIFI_EXPIRED_TIME);
+sleep(5);
+header('Location: ' . DEFAULT_URL);
---
+++ s/qq/index.php
@@ -0,0 +1,27 @@
+<?php
+
+session_start();
+
+$Mac_ID = isset($_GET['id']) ? addslashes($_GET['id']) : '';
+
+if (!$Mac_ID) {
+    header('Location: ' . DEFAULT_URL);
+    exit();
+}
+$_SESSION['Mac_ID'] = $Mac_ID;
+
+?>
+<!DOCTYPE html>
+<html lang="zh-cn">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+</head>
+
+<body>
+<!-- 授权按钮 -->
+<a href="auth.php"><img src="qq_login.png"></a>
+</body>
+</html>
