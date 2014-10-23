df3f9938f44a3ba8523e0ef275814dfd490d51ff
Eric Zhao <zhaoyijun1987@hotmail.com>
2014-10-23 09:45:15 +0800

    test

--- template/admin/create_site.php
+++ template/admin/create_site.php
@@ -2,5 +2,8 @@
 $title = 'site创建';
 $nav = 'nav.php';
 $content = 'create_site_content.php';
+$css_array = array(
+    0 => 'admin.css'
+);
 include_once('frame.php');
 
--- template/module/header.php
+++ template/module/header.php
@@ -9,6 +9,14 @@ include_once('../../config.php');
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="<?php echo BOOTSTRAP_CSS_PATH ?>/bootstrap.min.css">
     <link rel="stylesheet" href="<?php echo BOOTSTRAP_CSS_PATH ?>/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="<?php echo CSS_PATH ?>/reset.css">
+    <?php
+        if (isset($css_array) && is_array($css_array) && count($css_array) > 0) {
+            foreach($css_array as $k => $v) {
+                echo '<link rel="stylesheet" href="' . CSS_PATH . '/' . $v . '">';
+            }
+        }
+    ?>
     <script src="<?php echo JQUERY_PATH ?>/jquery.min.js"></script>
     <script src="<?php echo BOOTSTRAP_JS_PATH ?>/bootstrap.min.js"></script>
     <title><?php echo (isset($title) ? $title : 'ubnt');?></title>
