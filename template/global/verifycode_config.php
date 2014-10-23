181f1810b6fd14deedc2c8348eef0d37e3269fd6
Eric Zhao <zhaoyijun1987@hotmail.com>
2014-10-23 17:52:41 +0800

    add auth config form

---
+++ template/global/verifycode_config.php
@@ -0,0 +1,12 @@
+<script id="verifycode-template" type="x-tmpl-mustache">
+<div id="verifycode-config" class="row">
+    <div class="col-xs-offset-1">
+        <div class="form-group">
+            <label for="verifycode_wifi_expired_time">授权过期时间（分钟）</label>
+            <input type="text" class="form-control" id="verifycode_wifi_expired_time" name="verifycode_wifi_expired_time" placeholder="如：60" >
+        </div>
+        <div class="form-group">
+            <label for="verifycode_default_url">默认跳转地址</label>
+            <input type="text" class="form-control" id="verifycode_default_url" name="verifycode_default_url" placeholder="如：http://www.ubnt.com.cn" >
+        </div>
+</script>
