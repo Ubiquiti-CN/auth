d399b33b521e9606cc8c665e4e7d5bf9138a470c
tru2dagame <tru.zhou@gmail.com>
2014-10-23 17:08:54 +0800

    add two mode "portal" and "config"

--- libraries/Bootstrap.php
+++ libraries/Bootstrap.php
@@ -5,8 +5,20 @@ class Bootstrap {
     private function __construct() {}
 
     public static function run() {
-        $query = Uri::call('site');
-        print_r($query);
+        $mode = Uri::call('mode');
+        switch ($mode) {
+            case 'portal':
+                $site = Uri::call('site');
+
+                break;
+            case 'config':
+
+                include TEMPLATE_PATH . "/admin/create_site.php";
+                break;
+            default:
+                break;
+        }
+
 
     }
 
