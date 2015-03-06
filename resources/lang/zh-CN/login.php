e8ba505dbcafb2509bfb30385ce351aafa077df5
Sid Wu <wubofengwbf@gmail.com>
2015-03-06 14:58:17 +0800

    locale zh

---
+++ resources/lang/zh-CN/login.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Author: Sid Wu
+ * Date: 15/3/6
+ * Time: 下午2:52
+ */
+
+return [
+    'rememberme' => '记住',
+    'forgotyourpassword' => '忘记密码？',
+    'email' => '邮箱',
+    'password' => '密码',
+    'login' => '登陆',
+    'register' => '注册'
+];
