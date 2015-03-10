583faa14878403903ff4499ae90338c4e0ed723a
Sid Wu <wubofengwbf@gmail.com>
2015-03-10 15:08:03 +0800

    unifi controller api version factory

--- app/Libraries/UniFiControllerApi.php
+++ app/Libraries/UniFiControllerApi.php
@@ -5,12 +5,35 @@
  * Time: 上午10:52
  */
 
-class UniFiControllerApi {
+class UniFiControllerApiFactory {
 
-    private $_cookie_file_path='';
-    public function __construct() {
-        $this->_cookie_file_path = Config::get('unifi.cookie_file_path');
+    private function __construct() {}
+
+    /*
+     * version 3.2.1&3.2.5 use ssl v3
+     * version 3.2.7&3.2.10 use ssl v1
+     */
+    public static function get_instance($version) {
+        switch ($version) {
+            case '3.2.1':
+            case '3.2.5':
+                return new UniFiControllerApiV321();
+                break;
+            case '3.2.7':
+            case '3.2.10':
+                return new UniFiControllerApiV327();
+                break;
+            default:
+                return new UniFiControllerApiV321();
+                break;
+        }
     }
+}
+
+abstract class UniFiControllerApi {
+
+    abstract public function get_cookie_file_path();
+    abstract public function get_ssl_version();
 
     public function login($server, $user, $password) {
         // Start Curl for login
@@ -18,12 +41,12 @@ class UniFiControllerApi {
         // We are posting data
         curl_setopt($ch, CURLOPT_POST, TRUE);
         // Set up cookies
-        curl_setopt($ch, CURLOPT_COOKIEJAR, $this->_cookie_file_path);
-        curl_setopt($ch, CURLOPT_COOKIEFILE, $this->_cookie_file_path);
+        curl_setopt($ch, CURLOPT_COOKIEJAR, $this->get_cookie_file_path());
+        curl_setopt($ch, CURLOPT_COOKIEFILE, $this->get_cookie_file_path());
         // Allow Self Signed Certs
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-        curl_setopt($ch, CURLOPT_SSLVERSION, 3);
+        curl_setopt($ch, CURLOPT_SSLVERSION, $this->get_ssl_version());
         // Login to the UniFi controller
         curl_setopt($ch, CURLOPT_URL, $server . "/login");
         curl_setopt($ch, CURLOPT_POSTFIELDS,
@@ -38,13 +61,12 @@ class UniFiControllerApi {
         // We are posting data
         curl_setopt($ch, CURLOPT_POST, TRUE);
         // Set up cookies
-        curl_setopt($ch, CURLOPT_COOKIEJAR, $this->_cookie_file_path);
-        curl_setopt($ch, CURLOPT_COOKIEFILE, $this->_cookie_file_path);
+        curl_setopt($ch, CURLOPT_COOKIEJAR, $this->get_cookie_file_path());
+        curl_setopt($ch, CURLOPT_COOKIEFILE, $this->get_cookie_file_path());
         // Allow Self Signed Certs
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-        // Force SSL3 only
-        curl_setopt($ch, CURLOPT_SSLVERSION, 3);
+        curl_setopt($ch, CURLOPT_SSLVERSION, $this->get_ssl_version());
         // Make the API Call
         curl_setopt($ch, CURLOPT_URL, $server . '/logout');
         curl_exec($ch);
@@ -66,13 +88,12 @@ class UniFiControllerApi {
         // We are posting data
         curl_setopt($ch, CURLOPT_POST, TRUE);
         // Set up cookies
-        curl_setopt($ch, CURLOPT_COOKIEJAR, $this->_cookie_file_path);
-        curl_setopt($ch, CURLOPT_COOKIEFILE, $this->_cookie_file_path);
+        curl_setopt($ch, CURLOPT_COOKIEJAR, $this->get_cookie_file_path());
+        curl_setopt($ch, CURLOPT_COOKIEFILE, $this->get_cookie_file_path());
         // Allow Self Signed Certs
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-        // Force SSL3 only
-        curl_setopt($ch, CURLOPT_SSLVERSION, 3);
+        curl_setopt($ch, CURLOPT_SSLVERSION, $this->get_ssl_version());
         // Make the API Call
         curl_setopt($ch, CURLOPT_URL, $server . '/api/s/' . $site . '/cmd/stamgr');
         curl_setopt($ch, CURLOPT_POSTFIELDS, 'json=' . $data);
@@ -96,13 +117,12 @@ class UniFiControllerApi {
         // We are posting data
         curl_setopt($ch, CURLOPT_POST, TRUE);
         // Set up cookies
-        curl_setopt($ch, CURLOPT_COOKIEJAR, $this->_cookie_file_path);
-        curl_setopt($ch, CURLOPT_COOKIEFILE, $this->_cookie_file_path);
+        curl_setopt($ch, CURLOPT_COOKIEJAR, $this->get_cookie_file_path());
+        curl_setopt($ch, CURLOPT_COOKIEFILE, $this->get_cookie_file_path());
         // Allow Self Signed Certs
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-        // Force SSL3 only
-        curl_setopt($ch, CURLOPT_SSLVERSION, 3);
+        curl_setopt($ch, CURLOPT_SSLVERSION, $this->get_ssl_version());
         // Make the API Call
         curl_setopt($ch, CURLOPT_URL, $server . '/api/cmd/stamgr');
         curl_setopt($ch, CURLOPT_POSTFIELDS, 'json=' . $data);
@@ -110,6 +130,58 @@ class UniFiControllerApi {
         curl_close($ch);
 
         $this->logout($server);
+    }
+
+    public function get_all_sites($server, $user, $password) {
+        $this->login($server, $user, $password);
+
+        // Send user to unauthorize and the time allowed
+        $data = json_encode(
+            array(
+                'cmd' => 'get-sites',
+            )
+        );
+        $ch = curl_init();
+        // We are posting data
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+        // Set up cookies
+        curl_setopt($ch, CURLOPT_COOKIEJAR, $this->get_cookie_file_path());
+        curl_setopt($ch, CURLOPT_COOKIEFILE, $this->get_cookie_file_path());
+        // Allow Self Signed Certs
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+        curl_setopt($ch, CURLOPT_SSLVERSION, $this->get_ssl_version());
+        // Make the API Call
+        curl_setopt($ch, CURLOPT_URL, $server . '/api/s/default/cmd/sitemgr');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, 'json=' . $data);
+        curl_exec($ch);
+        curl_close($ch);
+
+        $this->logout($server);
+    }
+
+}
+
+class UniFiControllerApiV321 extends UniFiControllerApi {
+
+    public function get_cookie_file_path() {
+        return Config::get('unifi.cookie_file_path');
+    }
+
+    public function get_ssl_version() {
+        return 3;
+    }
+
+}
+
+class UniFiControllerApiV327 extends UniFiControllerApi {
+
+    public function get_cookie_file_path() {
+        return Config::get('unifi.cookie_file_path');
+    }
+
+    public function get_ssl_version() {
+        return 1;
     }
 
 }
