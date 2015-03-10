f01d187430289e8680f4be618b6065135b9f9049
Sid Wu <wubofengwbf@gmail.com>
2015-03-10 16:36:32 +0800

    test get sites

--- app/Http/Controllers/Site/ConfigController.php
+++ app/Http/Controllers/Site/ConfigController.php
@@ -24,6 +24,7 @@ class ConfigController extends Controller {
         $model = new GlobalConfig();
         $config = $model::where('user_id', '=', $user_id)->first();
         $global_config = is_object($config) ? json_decode($config->data, true) : array();
+        echo "<pre>";print_r($global_config);
         // todo judge global config
         $unifi_version = $global_config['controllerVersion'];
         $unifi_host = $global_config['controllerHost'];
@@ -32,7 +33,7 @@ class ConfigController extends Controller {
 
         $api_factory = UniFiControllerApiFactory::get_instance($unifi_version);
         $sites = $api_factory->get_all_sites($unifi_host, $unifi_user, $unifi_password);
-
+        print_r($sites);exit;
         return view('site/list', ['sites' => $sites]);
 	}
 
