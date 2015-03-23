5cac16510dc74ed8b18ad04a791a2475c84ee29a
tru2dagame <tru.zhou@gmail.com>
2015-03-23 18:11:34 +0800

    增加 password_logs 表的字段

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit78a073c86659fd0dac891d782fbecc6a::getLoader();
+return ComposerAutoloaderInitb461e502d1418c61b9d5a4cce0a7f375::getLoader();
